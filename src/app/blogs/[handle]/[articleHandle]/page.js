5dba2714c03418d98ecafc18dc19b47295d20bcf
SMHASSANALI <smhassanali@gmail.com>
2025-09-17 20:52:32 +0500

    Minor Update

--- src/app/blogs/[handle]/[articleHandle]/page.js
+++ src/app/blogs/[handle]/[articleHandle]/page.js
@@ -58,7 +58,7 @@ export default async function ArticlePage({ params }) {
             Article Not Found
           </h1>
           <p className="text-black text-lg">
-            Sorry, the article you are looking for does not exist.
+            Sorry, the article you are looking for does not exist or has been removed.
           </p>
           <Link
             href="/blogs"
